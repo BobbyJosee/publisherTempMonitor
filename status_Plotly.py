0e5d2cf2df7411af0b879cc465f8f14ec6ea1b2a
BobbyJosee <bobbyjosef35@gmail.com>
2019-01-27 14:34:47 +0530

    Add existing file

--- status_Plotly.py
+++ status_Plotly.py
@@ -19,11 +19,11 @@ os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
 base_dir = '/sys/bus/w1/devices/'
-device_file_list = [glob.glob(base_dir + '28*')[0] + '/w1_slave',glob.glob(base_dir + '28*')[1] + '/w1_slave']
+device_file_list = glob.glob(base_dir + '28*')[0] + '/w1_slave'
 
-script_path = "/home/pi/PiPonics/"
-pump_file = script_path + "pump.txt"
-drain_file = script_path + "drain.txt"
+#script_path = "/home/pi/PiPonics/"
+#pump_file = script_path + "pump.txt"
+#drain_file = script_path + "drain.txt"
 
 #Callback function on message receive
 def on_message(client,userdata,message):
@@ -52,25 +52,25 @@ print("connecting to broker: " + broker_address)
 client.connect(broker_address)
 
 #temperatue raw
-def read_temp_raw(device_file_no):
-	device_file_current=device_file_list[device_file_no]
+def read_temp_raw():
+	device_file_current=device_file_list
 	f = open(device_file_current, 'r')
 	lines = f.readlines()
 	f.close()
 	return lines
 
 #temperature
-def read_temp(device_file_no):
-    lines = read_temp_raw(device_file_no)
+def read_temp():
+    lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
-        lines = read_temp_raw(device_file_no)
+        lines = read_temp_raw()
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return round(temp_f,0)
+        return round(temp_c,0)
 
 try:
     client.loop_start()
@@ -79,27 +79,13 @@ try:
     client.subscribe(sub_topic)
     while True:
 	timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-	pump_status=0
-	drain_status=0
-	
-	with open(pump_file, "r") as fo:
-				fo.seek(0, 0)
-				pump_status = fo.read(1)
-	fo.closed
-	
-	with open(drain_file, "r") as fo:
-				fo.seek(0, 0)
-				drain_status = fo.read(1)
-	fo.closed
 	
 	print ("checking temp and posting")
-	print (timestamp + 'Temp 1:' + str(read_temp(0)) + ' Temp 2:' + str(read_temp(1)) + ' Pump:' + str(pump_status) + ' Drain:' + str(drain_status))
-        temperature_1 = read_temp(0)
-        temperature_2 = read_temp(1)
+	print (timestamp + 'Temp 1:' + str(read_temp()))
+        temperature_1 = read_temp()
         list = [timestamp,str(temperature_1)]
 	data = json.dumps(list)
-        print (str(temperature_1) + str(temperature_2))
+        print (str(temperature_1))
         print("Publish to Topic" + pub_topic)
 #punblish to topic
         client.publish(pub_topic, data)
@@ -107,5 +93,6 @@ try:
 	print("Sleeping")
 	time.sleep(10)
 
-except:
+except Exception as e:
        client.loop_stop()
+       print e
